36f55c2e9988685f1a6d3aa9e73048c878b0268a
ponnl <lakshmitharun.ponnam@in.pega.com>
2020-08-25 16:18:18 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
+import Comments from "./Comments";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <Comments/>
+            <Comments/>
+            <Comments/>
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
+
